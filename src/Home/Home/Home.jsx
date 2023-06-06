1cb197c150a7777df5470a1e975123b554d865f4
z3san <ahtisamhaque@gmail.com>
2023-06-07 00:37:59 +0600

    navbar created

---
+++ src/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const Home = () => {
+    return (
+        <div>
+        <Helmet>
+        <title>FluentFun | Home</title>
+      </Helmet>
+            home
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { HelmetProvider } from 'react-helmet-async';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<HelmetProvider>
+<div className="max-w-screen-xl mx-auto">
+  <RouterProvider router={router} />
+</div>
+</HelmetProvider>
+
+
+
+)
