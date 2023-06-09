a5076ce114d107cab690432af91f8ff709086298
z3san <ahtisamhaque@gmail.com>
2023-06-09 12:42:58 +0600

    userole hook created

---
+++ src/Hooks/useRole.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./UseAuthentication";
+
+const useRole = () => {
+  const { user } = useAuth();
+  const { data = {} } = useQuery({
+    queryKey: ["role", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/role?email=${user?.email}`
+      );
+      const result = await res.json();
+      return result;
+    },
+  });
+
+  return { data };
+};
+
+export default useRole;
