a361280cfd56ea5a011f41def28374bd2a753929
z3san <ahtisamhaque@gmail.com>
2023-06-12 17:27:14 +0600

    react awesome reveal installed on sidebar

--- src/Pages/Dashboard/Sidebar.jsx
+++ src/Pages/Dashboard/Sidebar.jsx
@@ -2,14 +2,12 @@ import React, { useContext, useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { AiOutlineBars } from 'react-icons/ai';
 import useRole from '../../Hooks/useRole';
-
-import { FaArrowLeft, FaHome } from 'react-icons/fa';
+import { Fade } from 'react-awesome-reveal';
+import { FaArrowLeft, FaHome, FaUsers, FaCog, FaPlus, FaChalkboardTeacher, FaListAlt, FaCheckSquare } from 'react-icons/fa';
 
 const Sidebar = () => {
-
   const [isActive, setActive] = useState(false);
-  const {data} = useRole()
-
+  const { data } = useRole();
 
   // Sidebar Responsive Handler
   const handleToggle = () => {
@@ -18,85 +16,101 @@ const Sidebar = () => {
 
   const admin = (
     <>
-     <h1 className='text-center'>Admin Panel</h1> <Link to='/'><FaArrowLeft className='text-white'/></Link>
-    <div className='divider' ></div>
-      <li>
-        <NavLink
-          to="/dashboard/manageUsers"
-          className={({ isActive }) => (isActive ? 'text-white' : 'default')}
-        >
-          Manage Users
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/dashboard/manageClasses"
-          className={({ isActive }) => (isActive ? 'text-white' : 'default')}
-        >
-          Manage Classes
-        </NavLink>
-      </li>
+      <Fade cascade direction="right">
+        <h1 className='text-center'>Admin Panel</h1>
+        <Link to='/'>
+          <FaArrowLeft className='text-white' />
+        </Link>
+        <div className='divider'></div>
+        <li>
+          <NavLink
+            to="/dashboard/manageUsers"
+            className={({ isActive }) => (isActive ? 'text-white' : 'default')}
+          >
+            <FaUsers className='icon' />
+            Manage Users
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/dashboard/manageClasses"
+            className={({ isActive }) => (isActive ? 'text-white' : 'default')}
+          >
+            <FaCog className='icon' />
+            Manage Classes
+          </NavLink>
+        </li>
+      </Fade>
     </>
   );
 
   const instructor = (
     <>
-    <h1 className='text-center'>Instructors Panel</h1>
-    <div className='divider' ></div>
-      <li>
-        <NavLink
-          to="/dashboard/addaclass"
-          className={({ isActive }) => (isActive ? 'text-white' : 'default')}
-        >
-          Add A Class
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/dashboard/myclass"
-          className={({ isActive }) => (isActive ? 'text-white' : 'default')}
-        >
-          My Classes
-        </NavLink>
-      </li>
+      <Fade cascade direction="right">
+        <h1 className='text-center'>Instructors Panel</h1>
+        <div className='divider'></div>
+        <li>
+          <NavLink
+            to="/dashboard/addaclass"
+            className={({ isActive }) => (isActive ? 'text-white' : 'default')}
+          >
+            <FaPlus className='icon' />
+            Add A Class
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/dashboard/myclass"
+            className={({ isActive }) => (isActive ? 'text-white' : 'default')}
+          >
+            <FaChalkboardTeacher className='icon' />
+            My Classes
+          </NavLink>
+        </li>
+      </Fade>
     </>
   );
 
   const student = (
     <>
-     <h1 className='text-center'>Students DashBoard </h1>
-    <div className='divider' ></div>
-      <li>
-        <NavLink
-          to="/dashboard/myselectedclass"
-          className={({ isActive }) => (isActive ? 'text-white' : 'default')}>
-          Selected Classes
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/dashboard/myenrolledclass"
-          className={({ isActive }) => (isActive ? 'text-white' : 'default')}
-        >
-          Enrolled Classes
-        </NavLink>
-      </li>
+      <Fade cascade direction="right">
+        <h1 className='text-center'>Students Dashboard</h1>
+        <div className='divider'></div>
+        <li>
+          <NavLink
+            to="/dashboard/myselectedclass"
+            className={({ isActive }) => (isActive ? 'text-white' : 'default')}
+          >
+            <FaListAlt className='icon' />
+            Selected Classes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/dashboard/myenrolledclass"
+            className={({ isActive }) => (isActive ? 'text-white' : 'default')}
+          >
+            <FaCheckSquare className='icon' />
+            Enrolled Classes
+          </NavLink>
+        </li>
+      </Fade>
     </>
   );
 
   return (
     <>
       {/* Small Screen Navbar */}
-      <div className="bg-gray-100 text-gray-800 flex justify-between md:hidden">
+      <div className='bg-gray-100 text-gray-800 flex justify-between md:hidden'>
         <div>
-          <div className="block cursor-pointer p-4 font-bold"></div>
+          <div className='block cursor-pointer p-4 font-bold'></div>
         </div>
 
         <button
           onClick={handleToggle}
-          className="mobile-menu-button p-4 focus:outline-none focus:bg-gray-200"
+          className='mobile-menu-button p-4 focus:outline-none focus:bg-gray-200'
         >
-          <AiOutlineBars className="h-5 w-5" />
+          <AiOutlineBars className='h-5 w-5' />
         </button>
       </div>
       {/* Sidebar */}
@@ -107,11 +121,14 @@ const Sidebar = () => {
       >
         <ul>
           {data.role === 'admin' && admin}
-          {data.role === 'instructor' &&  instructor}
-          { data.role === 'student' && student }
+          {data.role === 'instructor' && instructor}
+          {data.role === 'student' && student}
         </ul>
         <div>
-         <Link className='flex items-center text-white gap-3 justify-center' to='/'><FaHome/>Back To Home</Link>
+          <Link className='flex items-center text-white gap-3 justify-center' to='/'>
+            <FaHome className='icon' />
+            Back To Home
+          </Link>
         </div>
       </div>
     </>
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,9 +1,6 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router";
 import { AuthContext } from "../Providers/AuthProviders";
-
-
-
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
